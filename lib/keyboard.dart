8ea0db147900745ad31b423f3da21ca4820cae76
Yehor Nestin <egornestinn@gmail.com>
2020-10-30 15:51:45 +0200

    option to remove keyboard item border on fill + allow to specify fill color

--- lib/keyboard.dart
+++ lib/keyboard.dart
@@ -10,6 +10,8 @@ class KeyboardUIConfig {
   final TextStyle deleteButtonTextStyle;
   final Color primaryColor;
   final Color digitFillColor;
+  final Color fillColor;
+  final bool fillOnTap;
   final EdgeInsetsGeometry keyboardRowMargin;
   final EdgeInsetsGeometry digitInnerMargin;
   //Size for the keyboard can be define and provided from the app. If it will not be provided the size will be adjusted to a screen size.
@@ -21,13 +23,15 @@ class KeyboardUIConfig {
     this.digitInnerMargin = const EdgeInsets.all(24),
     this.primaryColor = Colors.white,
     this.digitFillColor = Colors.transparent,
+    this.fillColor,
+    this.fillOnTap = false,
     this.digitTextStyle = const TextStyle(fontSize: 30, color: Colors.white),
     this.deleteButtonTextStyle = const TextStyle(fontSize: 16, color: Colors.white),
     this.keyboardSize,
   });
 }
 
-class Keyboard extends StatelessWidget {
+class Keyboard extends StatefulWidget {
   final KeyboardUIConfig keyboardUIConfig;
   final KeyboardTapCallback onKeyboardTap;
 
@@ -42,20 +46,43 @@ class Keyboard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _KeyboardState createState() => _KeyboardState();
+}
+
+class _KeyboardState extends State<Keyboard> {
+  List<bool> _keyboardItemsFilledState = List.filled(10, false);
+
+  Color get _keyboardHighlightColor {
+    if (widget.keyboardUIConfig.fillOnTap) {
+      return widget.keyboardUIConfig.fillColor ?? widget.keyboardUIConfig.primaryColor;
+    }
+
+    return null;
+  }
+
+  Color get _keyboardSplashColor {
+    if (widget.keyboardUIConfig.fillOnTap) {
+      return Colors.transparent;
+    }
+
+    return (widget.keyboardUIConfig.fillColor ?? widget.keyboardUIConfig.primaryColor).withOpacity(0.4);
+  }
+
+  @override
   Widget build(BuildContext context) => _buildKeyboard(context);
 
   Widget _buildKeyboard(BuildContext context) {
     List<String> keyboardItems = List.filled(10, '0');
-    if (digits == null || digits.isEmpty) {
+    if (widget.digits == null || widget.digits.isEmpty) {
       keyboardItems = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     } else {
-      keyboardItems = digits;
+      keyboardItems = widget.digits;
     }
     final screenSize = MediaQuery.of(context).size;
     final keyboardHeight = screenSize.height > screenSize.width ? screenSize.height / 2 : screenSize.height - 80;
     final keyboardWidth = keyboardHeight * 3 / 4;
-    final keyboardSize = this.keyboardUIConfig.keyboardSize != null
-        ? this.keyboardUIConfig.keyboardSize
+    final keyboardSize = widget.keyboardUIConfig.keyboardSize != null
+        ? widget.keyboardUIConfig.keyboardSize
         : Size(keyboardWidth, keyboardHeight);
     return Container(
       width: keyboardSize.width,
@@ -64,34 +91,48 @@ class Keyboard extends StatelessWidget {
       child: AlignedGrid(
         keyboardSize: keyboardSize,
         children: List.generate(10, (index) {
-          return _buildKeyboardDigit(keyboardItems[index]);
+          return _buildKeyboardDigit(keyboardItems[index], index);
         }),
       ),
     );
   }
 
-  Widget _buildKeyboardDigit(String text) {
+  Widget _buildKeyboardDigit(String text, int index) {
     return Container(
       margin: EdgeInsets.all(4),
       child: ClipOval(
         child: Material(
-          color: keyboardUIConfig.digitFillColor,
+          color: widget.keyboardUIConfig.digitFillColor,
           child: InkWell(
-            splashColor: keyboardUIConfig.primaryColor.withOpacity(0.4),
+            highlightColor: _keyboardHighlightColor,
+            splashColor: _keyboardSplashColor,
             onTap: () {
-              onKeyboardTap(text);
+              setState(() {
+                _keyboardItemsFilledState[index] = false;
+              });
+              widget.onKeyboardTap(text);
+            },
+            onTapDown: (event) {
+              setState(() {
+                _keyboardItemsFilledState[index] = true;
+              });
             },
             child: Container(
               child: Center(
                 child: Text(
                   text,
-                  style: keyboardUIConfig.digitTextStyle,
+                  style: widget.keyboardUIConfig.digitTextStyle,
                   semanticsLabel: text,
                 ),
               ),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                border: Border.all(color: keyboardUIConfig.primaryColor, width: keyboardUIConfig.digitBorderWidth),
+                border: Border.all(
+                  color: widget.keyboardUIConfig.fillOnTap && _keyboardItemsFilledState[index]
+                      ? Colors.transparent
+                      : widget.keyboardUIConfig.primaryColor,
+                  width: widget.keyboardUIConfig.digitBorderWidth
+                ),
               ),
             ),
           ),
